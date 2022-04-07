c54c8660a82cf92f4656fec73bf83f22a387d49e
Bastian <reuther-bastian@web.de>
2022-04-07 20:49:01 +0200

    init

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//Test
+
+
+
 /**
  * T-Rex runner.
  * @param {string} outerContainerId Outer containing element id.
